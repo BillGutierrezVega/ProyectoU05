22e8b59f0fbcc54c732dcbd40c472dd0f9de0b39
BillGutierrezVega <darckneck@hotmail.com>
2022-12-20 22:20:14 -0500

    esqueleto a

--- Ventapp/Ventapp/settings.py
+++ Ventapp/Ventapp/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'versioned.apps.VersionedConfig',
+    'pagos.apps.PagosConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
