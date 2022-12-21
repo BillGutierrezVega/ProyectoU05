9d8a860a721fed1d4201b68ddbcb889fca48b006
BillGutierrezVega <darckneck@hotmail.com>
2022-12-21 00:14:29 -0500

    un avance

---
+++ Ventapp/users/urls.py
@@ -0,0 +1,11 @@
+# from rest_framework import routers
+
+# from django.urls import path
+
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+# from users import views
+
+
+# router = routers.DefaultRouter(sliding_slash=False)
+# router.register('', views.GetUsers)
--- Ventapp/users/views.py
+++ Ventapp/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render, redirect
+# from django.contrib.auth import authenticated
+
+# from rest_framework import Request, Response, viewsets
+# from rest_framework.views import APIView
+
+# from users.serializers import RegistroSerializer, GetUserSerializer
+# from users.tokens import create_jwt_for_user
+# from users.models import User
+
+
 
 # Create your views here.
